71e9be3976504f05c5a24922ddd24ff99a347c27
Alicia <arrowsrp@yahoo.com>
2022-09-01 13:23:27 -0700

    Creates skaters table migration'

---
+++ db/migrate/20220901201656_skaters.rb
@@ -0,0 +1,11 @@
+class Skaters < ActiveRecord::Migration[6.1]
+  def change
+    create_table :skaters do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :category_id
+      t.boolean :checkedIn?
+      t.integer :skatepark_id
+    end
+  end
+end
