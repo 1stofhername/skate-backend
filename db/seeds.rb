d50168ea4b6ee1f7a3a4f486a00aac8730426b07
Alicia <arrowsrp@yahoo.com>
2022-09-01 19:18:10 -0700

    Adds seed data for category table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,35 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+categories = ["skateboard", "inlineskate", "rollerskate", "scooter","other"]
+
+def fake_first_name
+    Faker::Name.first_name
+end
+
+def fake_last_name
+    Faker::Name.last_name
+end
+
+# 10.times do
+#     first_name = fake_first_name()
+#     last_name = fake_last_name()
+#     User.create(
+#         first_name: first_name,
+#         last_name: last_name,
+#         email: "#{first_name}.#{last_name}@email.com",
+#         category: ["jet-setter", "vanlifer", "traveler"][rand(0..2)],
+#         password: Faker::Internet::password,
+#         follower_id: rand(1..10)
+#     )
+# end
+
+categories.map do |c|
+    Category.create(
+        category_name:c
+    )
+end
+
+
 
 puts "✅ Done seeding!"
