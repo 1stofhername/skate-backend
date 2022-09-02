391fa51b617c7daf0c9392ba00789b942ce12145
Alicia <arrowsrp@yahoo.com>
2022-09-01 18:39:00 -0700

    Changes skaters table to users

--- db/migrate/20220901201656_create_users.rb
+++ db/migrate/20220901201656_create_users.rb
@@ -1,13 +1,12 @@
-class Skaters < ActiveRecord::Migration[6.1]
+class Users < ActiveRecord::Migration[6.1]
   def change
-    create_table :skaters do |t|
+    create_table :users do |t|
       t.string :first_name
       t.string :last_name
       t.string :email
       t.string :password
       t.string :category_id
       t.boolean :checkedIn?
-      t.integer :skatepark_id
     end
   end
 end
