3d5c0241581d04361a363ebddfbc3e17b0969a82
Alicia <arrowsrp@yahoo.com>
2022-09-01 18:20:46 -0700

    Adds columns to skater table migration file

--- db/migrate/20220901201656_create_skaters.rb
+++ db/migrate/20220901201656_create_skaters.rb
@@ -3,6 +3,8 @@ class Skaters < ActiveRecord::Migration[6.1]
     create_table :skaters do |t|
       t.string :first_name
       t.string :last_name
+      t.string :email
+      t.string :password
       t.string :category_id
       t.boolean :checkedIn?
       t.integer :skatepark_id
